f52e4744d6b4ae3bf974d97479da31d7875e3bf9
Janki3010 <jankipatel9012@gmail.com>
2025-04-15 16:28:10 +0530

    Add files via upload

---
+++ house_prediction.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pd.read_csv("house_pr.csv")
+print(df)
+
+plt.xlabel('area')
+plt.ylabel('price')
+plt.scatter(df.area, df.price, color='red', marker='+')
+plt.show()
+
+new_df = df.drop('price', axis='columns')
+print('area\n', new_df)
+price = df.price
+print('price\n', price)
+
+# Create linear regression object
+reg = linear_model.LinearRegression()
+reg.fit(new_df,price)
+
+print("Predicted Price:", reg.predict([[3300]])[0]) 
+reg_coef = reg.coef_[0]
+reg_intercept = reg.intercept_
+
+""" Formula to find predicted price
+Y = m * X + b (X is area , m is coefficient and b is intercept) """
+print(reg_coef*3300+reg_intercept)
+
+# Generate CSV file with list of home price predication
+area_df = pd.read_csv("area.csv")
+area_df.head(3)
+
+p = reg.predict(area_df)
+print("all area prediction:", p)
+
+area_df['price'] = p
+print(area_df)
+
+area_df.to_csv("prediction.csv")
